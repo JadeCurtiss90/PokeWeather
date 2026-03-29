0b600d7596cf110df75f00a3fe1b2fb00c7ef20b
JadeCurtiss90 <jadecurtiss90@gmail.com>
2026-03-29 03:41:22 -0400

    Proverbial whack-a-mole r2 (catching more specific errors)

--- APILink.py
+++ APILink.py
@@ -17,11 +17,18 @@ def get_api_data(url, apikey = None, params = ""):
 def get_coordinates(city, state) :
     try:
         location = Nominatim(user_agent="WeatherLightApp").geocode(f"{city}, {state}", country_codes="us")
-        print(location.address)
         return location.latitude, location.longitude
+    except AttributeError as e:
+        if location is None:
+            print(f"No location found!")
+        else:
+            print(f"An error occurred: {e}")
+            print(f"No location found!")
+        # if no location is found, default to Nowhere
+        return 35.159167, -98.442222
     except Exception as e:
         print(f"An error occurred: {e}")
-        print(f"Unable to determine current location")
+        print(f"Unable to determine location")
         # if no location is found, default to Nowhere
         return 35.159167, -98.442222
 
